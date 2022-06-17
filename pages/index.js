a5226c15ca585aca46c13ac2e1bbe60f42503e97
Amirmansour97 <74016818+Amirmansour97@users.noreply.github.com>
2022-06-18 00:01:46 +0300

    Last Edit

--- pages/index.js
+++ pages/index.js
@@ -74,8 +74,8 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
       <div className="pt-10 pb-6 flex flex-col items-center">
         <h2 className="text-4xl mb-3">Trending Now</h2>
         <p className="text-gray-600 text-sm">
-          Find the perfect piece or accessory to finish off your favorite room
-          in the house.
+          Find the perfect piece or accessory to finish off your Vehicle in Your
+          Garage.
         </p>
       </div>
       <div className="my-8 flex flex-col lg:flex-row justify-between">
